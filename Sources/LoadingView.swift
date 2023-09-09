e3fb686b3907f104f2985d8a6606bbb7cba9db81
yuriynefedov <theyuriynefeedov@gmail.com>
2023-09-09 20:58:44 +0300

    Improve color-related code

--- Sources/LoadingView.swift
+++ Sources/LoadingView.swift
@@ -39,8 +39,15 @@ public struct Sector: Shape {
 public struct LoadingView: View {
     public var style: Style = .spinner
     public var progress: Double = Self.defaultProgress
+    public var primaryColor: Color = Self.defaultPrimaryColor
+    public var secondaryColor: Color = Self.defaultSecondaryColor
     
-    public init(style: Style, progress: Double) {
+    public init(
+        style: Style,
+        progress: Double,
+        primaryColor: Color = Self.defaultPrimaryColor,
+        secondaryColor: Color = Self.defaultSecondaryColor
+    ) {
         self.style = style
         self.progress = progress
     }
@@ -62,12 +69,12 @@ public struct LoadingView: View {
         let stroke: CGFloat = 6
         ZStack {
             Circle()
-                .stroke(Self.primaryColor, lineWidth: stroke)
+                .stroke(primaryColor, lineWidth: stroke)
                 .foregroundColor(.clear)
             Sector(percentage: progress)
         }
         .frame(maxWidth: maxSize, maxHeight: maxSize)
-        .foregroundColor(Self.primaryColor)
+        .foregroundColor(primaryColor)
     }
     
     @ViewBuilder
@@ -79,11 +86,11 @@ public struct LoadingView: View {
                 RoundedRectangle(cornerRadius: height/2)
                     .frame(height: height)
                     .frame(width: min(maxWidth, proxy.size.width))
-                    .foregroundColor(.secondary)
+                    .foregroundColor(secondaryColor)
                 RoundedRectangle(cornerRadius: height/2)
                     .frame(height: height)
                     .frame(width: min(maxWidth, proxy.size.width)*progress)
-                    .foregroundColor(.primary)
+                    .foregroundColor(primaryColor)
             }
         }
         .frame(maxWidth: maxWidth)
@@ -97,8 +104,20 @@ extension LoadingView {
         case cirular
     }
     public static var defaultProgress: Double { 0.66 }
-    private static var primaryColor: Color { .primary }
-    private static var secondaryColor: Color { .secondary }
+    public static var defaultPrimaryColor: Color {
+        .init(
+            red: 0.612,
+            green: 0.612,
+            blue: 0.612
+        )
+    }
+    public static var defaultSecondaryColor: Color {
+        .init(
+            red: 0.871,
+            green: 0.871,
+            blue: 0.871
+        )
+    }
 }
 
 struct LoadingView_Previews: PreviewProvider {
